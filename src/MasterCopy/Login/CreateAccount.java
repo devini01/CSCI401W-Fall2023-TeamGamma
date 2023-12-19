7748019fee3117bb9d346861dc51e6d03d887d49
devini01 <144073896+devini01@users.noreply.github.com>
2023-12-19 16:22:04 -0500

    Update CreateAccount.java

--- src/MasterCopy/Login/CreateAccount.java
+++ src/MasterCopy/Login/CreateAccount.java
@@ -2,41 +2,22 @@ package org.meicode.rafflescreennew;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.util.Patterns;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.annotation.NonNull;
-import com.google.firebase.Firebase;
 
 public class CreateAccount extends AppCompatActivity {
 
     EditText editTextEmail, editTextPassword, createPass2;
     Button buttonCreate;
-    FirebaseAuth mAuth; // Creates an objec to reference the database
-
-/*    @Override
-    public void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
-            finish();
-        }
-    }*/
+    FirebaseAuth mAuth; // Creates an object to reference the database
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,10 +78,4 @@ public class CreateAccount extends AppCompatActivity {
             return false;
         }
     }
-
-
-
-
-
-
 }
